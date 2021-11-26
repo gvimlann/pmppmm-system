e0b5fe0cbe6dcbc5129569d4de7288823d6b4b19
Vimlan G <minedmind10@gmail.com>
2021-11-26 20:35:14 +0800

    login page protected route:

--- server/controllers/util.js
+++ server/controllers/util.js
@@ -1,6 +1,13 @@
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
+
+const awsConfig = {
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
+	apiVersion: process.env.AWS_API_VERSION,
+};
 
 export const sendEmail = async (req, res) => {
-  console.log("Send Email");
-  res.json({ success: true });
+	console.log('Send Email');
+	res.json({ success: true });
 };
